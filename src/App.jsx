944050de0a7344e9269e1ba0e5bbfd6d08c437bd
Moses <mosesjayasinghp@gmail.com>
2026-07-07 12:03:09 +0530

    Completed Task 20

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Counter Application</h1>
+
+      <h2>Count: {count}</h2>
+
+      <div className="buttons">
+        <button onClick={() => setCount(count + 1)}>
+          Increment
+        </button>
+
+        <button onClick={() => setCount(count - 1)}>
+          Decrement
+        </button>
+
+        <button onClick={() => setCount(0)}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
